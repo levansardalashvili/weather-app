a93dd674e2f8d68055d40068f7c80048ce0b61f8
levansardalashvili <sardalaalevani@gmail.com>
2022-01-20 23:18:16 +0400

    Updates

---
+++ precache-manifest.2742ca6d7f8a8e3034ef26e0de50bd12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271371c44ab4ffc014537b87bb2bcc91",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ccbe12e31a9de56aab52",
+    "url": "/weather-app/static/css/main.1fe71e9d.chunk.css"
+  },
+  {
+    "revision": "44d91217a2106cc453b8",
+    "url": "/weather-app/static/js/2.6fc8ad99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-app/static/js/2.6fc8ad99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccbe12e31a9de56aab52",
+    "url": "/weather-app/static/js/main.fa4ca99d.chunk.js"
+  },
+  {
+    "revision": "e39fc71348408d27bfee",
+    "url": "/weather-app/static/js/runtime-main.8d0108a6.js"
+  },
+  {
+    "revision": "c60b5ebead2a5c800963973ff7c0d2da",
+    "url": "/weather-app/static/media/sum.c60b5ebe.jpg"
+  },
+  {
+    "revision": "00215c8811c11b8797bea6a36ec9e881",
+    "url": "/weather-app/static/media/wint.00215c88.jpg"
+  }
+]);
